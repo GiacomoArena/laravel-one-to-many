388893812975b54c98f9fd9872caab811812bcdf
GiacomoArena <giacomoarena28@gmail.com>
2023-06-21 15:35:00 +0200

    make model and migration

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container py-5">
-        <h1>HOME pubblica</h1>
+        <h1>HOME Pubblica!!</h1>
         <a class="btn btn-dark" href="{{route('admin.home')}}">
             vai al login
         </a>
